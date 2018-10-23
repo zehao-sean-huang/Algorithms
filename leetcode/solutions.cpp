485f2de97a029c819e7776a056735b7dc1822765
Zehao Huang <1078351005@qq.com>
2018-10-23 11:14:35 -0700

    leetcode 64 70

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -21,13 +21,6 @@ class Solution {
     }
 
     /**
-     * Problem 5
-     */
-    string longestPalindrome(string s) {
-        // 
-    }
-
-    /**
      * Problem 32
      */
     int longestValidParentheses(string s) {
@@ -66,5 +59,41 @@ class Solution {
             }
         }
         return result[obstacleGrid.size()][obstacleGrid[0].size()];
+    }
+
+    /**
+     * Problem 64
+     */
+    int minPathSum(vector<vector<int>>& grid) {
+        int result[1010][1010];
+        memset(result, INT_MAX, sizeof(result));
+        result[0][1] = result[1][0] = grid[0][0];
+        for (int r = 0; r < grid.size(); ++r) {
+            for (int c = 0; c < grid[r].size(); ++c) {
+                if (r == 0 && c == 0) {
+                    result[r][c] = grid[r][c];
+                } else if (r == 0) {
+                    result[r][c] = grid[r][c] + result[r][c - 1];
+                } else if (c == 0) {
+                    result[r][c] = grid[r][c] + result[r - 1][c];
+                } else {
+                    result[r][c] = grid[r][c] + min(result[r][c - 1], result[r - 1][c]);
+                }
+            }
+        }
+        return result[grid.size() - 1][grid[0].size() - 1];
+    }
+
+    /**
+     * Problem 70
+     */
+    map<int, int> computed;
+    int climbStairs(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        } else if (computed.find(n) == computed.end()) {
+            computed[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        } 
+        return computed[n];
     }
 };
