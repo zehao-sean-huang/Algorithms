71a6113330d9f920c3a7759cd3b5181a7be272b5
GodfatherSean <1078351005@qq.com>
2018-10-25 01:44:34 -0700

    leetcode 87

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -10,8 +10,15 @@ void p44(Solution &solutions) {
     cout << solutions.isMatchWildcard("acdcb", "a*c?b") << endl;
 }
 
+void p87(Solution &s) {
+    cout << s.isScramble("great", "rgeat") << endl; // true
+    cout << s.isScramble("great", "rgtae") << endl; // true
+    cout << s.isScramble("abcde", "caebd") << endl; // false
+}
+
 int main() {
     Solution solutions = Solution();
-    p44(solutions);
+    // p44(solutions);
+    p87(solutions);
     return 0;
 }
