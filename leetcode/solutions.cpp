3f954f3cd4b0cb6eea0eee920e9679fa9b10ac09
GodfatherSean <1078351005@qq.com>
2018-10-25 18:29:24 -0700

    leetcode 121

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -510,4 +510,27 @@ class Solution {
         }
         return triangle[0][0];
     }
+
+    /**
+     * Problem 121
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+     * Note that you cannot sell a stock before you buy one.
+     */
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size(), maxp = 0;
+        int search[n + 10];
+        memset(search, -1, sizeof(search));
+        for (int i = 1; i <= n; ++i) {
+            search[i] = 0;
+            for (int j = i - 1; j >= 1; --j) {
+                if (prices[j - 1] < prices[i - 1]) {
+                    search[i] = prices[i - 1] - prices[j - 1] + search[i];
+                    maxp = max(maxp, search[i]);
+                    break;
+                }
+            }
+        }
+        return search[n];
+    }
 };
