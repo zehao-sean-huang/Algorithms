a19a067097c6e3da3c837da06750373f643457fa
Zehao Huang <1078351005@qq.com>
2018-10-25 17:17:16 -0700

    leetcode 97

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -437,4 +437,36 @@ class Solution {
         }
         return r[n];
     }
+
+    /**
+     * Problem 97
+     * Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+     */
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.size() + s2.size() != s3.size()) {
+            return false;
+        } else {
+            int m = s1.size(), n = s2.size();
+            bool r[m + 5][n + 5];
+            memset(r, false, sizeof(r));
+            r[0][0] = true;
+            for (int i = 1; i <= m; ++i) {
+                r[i][0] = (s1[i - 1] == s3[i - 1]) && r[i - 1][0];
+            }
+            for (int j = 1; j <= n; ++j) {
+                r[0][j] = (s2[j - 1] == s3[j - 1]) && r[0][j - 1];
+            }
+            for (int i = 1; i <= m; ++i) {
+                for (int j = 1; j <= n; ++j) {
+                    if (r[i - 1][j] && s1[i - 1] == s3[i + j - 1]) {
+                        r[i][j] = true;
+                    }
+                    if (r[i][j - 1] && s2[j - 1] == s3[i + j - 1]) {
+                        r[i][j] = true;
+                    }
+                }
+            }
+            return r[m][n];
+        }
+    }
 };
