423eb82c88d8ee28665fd951deb0420fff8ce69c
Zehao Huang <1078351005@qq.com>
2018-10-26 04:42:29 -0700

    leetcode 95

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -444,6 +444,44 @@ class Solution {
     }
 
     /**
+     * Problem 95
+     * Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+     */
+    vector<TreeNode*> generateTreesHelper(int start, int end) {
+        vector<TreeNode*> r;
+        if (start > end) {
+            r.push_back(nullptr);
+        } else if (start == end) {
+            TreeNode* t = new TreeNode(start);
+            r.push_back(t);
+        } else {
+            vector<TreeNode*> left;
+            vector<TreeNode*> right;
+            for (int i = start; i <= end; ++i) {
+                left = generateTreesHelper(start, i - 1);
+                right = generateTreesHelper(i + 1, end);
+                for (TreeNode* ln : left) {
+                    for (TreeNode* rn : right) {
+                        TreeNode* root = new TreeNode(i);
+                        root->left = ln;
+                        root->right = rn;
+                        r.push_back(root);
+                    }
+                }
+            }
+        }
+        return r;
+    }
+
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) {
+            vector<TreeNode*> r;
+            return r;
+        }
+        return generateTreesHelper(1, n);
+    }
+
+    /**
      * Problem 96
      * Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
      */
