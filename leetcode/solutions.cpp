219873769b8da0901529cf69a1671bfa6fdaf0b1
GodfatherSean <zehao.sean.huang@gmail.com>
2018-11-18 18:09:42 -0800

    leetcode 174

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -799,6 +799,34 @@ class Solution {
     }
 
     /**
+     * Problem 174
+     * The demons had captured the princess (P) and imprisoned her in the bottom-right corner of a dungeon. 
+     * The dungeon consists of M x N rooms laid out in a 2D grid. Our valiant knight (K) was initially positioned 
+     * in the top-left room and must fight his way through the dungeon to rescue the princess.
+     * 
+     * The knight has an initial health point represented by a positive integer. If at any point his health 
+     * point drops to 0 or below, he dies immediately.
+     * 
+     * Some of the rooms are guarded by demons, so the knight loses health (negative integers) upon entering 
+     * these rooms; other rooms are either empty (0's) or contain magic orbs that increase the knight's health (positive integers).
+     * 
+     * In order to reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.
+     */
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int m = dungeon.size(), n = dungeon[0].size();
+        int r[m + 10][n + 10];
+        memset(r, 1, sizeof(r));
+        r[m][n - 1] = 1; r[m - 1][n] = 1;
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                int temp = min(r[i + 1][j], r[i][j + 1]) - dungeon[i][j];
+                r[i][j] = temp <= 0 ? 1 : temp;
+            }
+        }
+        return r[0][0];
+    }
+
+    /**
      * Problem 198
      * You are a professional robber planning to rob houses along a street. 
      * Each house has a certain amount of money stashed, the only constraint stopping you from 
