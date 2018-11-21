e2c957cd5eba09972298e67ff66b8b0a79c80d0b
Zehao Huang <1078351005@qq.com>
2018-11-20 21:00:14 -0800

    leetcode 771

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -872,6 +872,25 @@ class Solution {
         }
         return min(ans1, ans2);
     }
+
+    /**
+     * Problem 771
+     * You're given strings J representing the types of stones that are jewels, and S representing 
+     * the stones you have.  Each character in S is a type of stone you have.  You want to know how 
+     * many of the stones you have are also jewels.
+     * 
+     * The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters 
+     * are case sensitive, so "a" is considered a different type of stone from "A".
+     */
+    int numJewelsInStones(string J, string S) {
+        int r = 0;
+        for (char s : S) {
+            if (J.find(s) != string::npos) {
+                r++;
+            }
+        }
+        return r;
+    }
     
     /**
      * Problem 814
