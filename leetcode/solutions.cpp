b0e2c0309919a8a9b3e768027eccc78eeafb5531
GodfatherSean <zehao.sean.huang@gmail.com>
2018-11-28 06:38:55 -0800

    leetcode 700

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -925,6 +925,24 @@ class Solution {
     }
 
     /**
+     * Problem 700
+     * Given the root node of a binary search tree (BST) and a value. You need to find 
+     * the node in the BST that the node's value equals the given value. Return the subtree 
+     * rooted with that node. If such node doesn't exist, you should return NULL.
+     */
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (root == nullptr) {
+            return root;
+        } else if (root->val == val) {
+            return root;
+        } else if (val < root->val) {
+            return searchBST(root->left, val);
+        } else {
+            return searchBST(root->right, val);
+        }
+    }
+
+    /**
      * Problem 701
      * Given the root node of a binary search tree (BST) and a value to be inserted into 
      * the tree, insert the value into the BST. Return the root node of the BST after the 
