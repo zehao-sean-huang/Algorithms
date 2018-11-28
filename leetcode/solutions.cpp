979fdd2a3adbee7a8410cd24927d00d950750387
GodfatherSean <zehao.sean.huang@gmail.com>
2018-11-28 07:17:11 -0800

    leetcode 111

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -693,6 +693,24 @@ class Solution {
     }
 
     /**
+     * Problem 111
+     * Minimum Depth of Binary Tree
+     */
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        } else if (root->left == nullptr && root->right == nullptr) {
+            return 1;
+        } else if (root->left == nullptr) {
+            return 1 + minDepth(root->right);
+        } else if (root->right == nullptr) {
+            return 1 + minDepth(root->left);
+        } else {
+            return 1 + min(minDepth(root->left), minDepth(root->right));
+        }
+    }
+
+    /**
      * Problem 112
      * Given a binary tree and a sum, determine if the tree has a 
      * root-to-leaf path such that adding up all the values along the path equals the given sum.
