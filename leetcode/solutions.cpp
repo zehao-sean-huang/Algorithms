e854df8dd80d98dc3eca5c39db6ee404c4db1626
GodfatherSean <zehao.sean.huang@gmail.com>
2018-11-30 22:22:56 -0800

    leetcode 429

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -851,6 +851,34 @@ class Solution {
     }
 
     /**
+     * Problem 429
+     */
+    vector<vector<int> > levelOrder(Node* root) {
+        vector<vector<int> > result;
+        if (root != nullptr) {
+            int count = 1;
+            queue<Node*> q;
+            q.push(root);
+            while (!q.empty()) {
+                int nextCount = 0;
+                vector<int> row;
+                for (int i = 0; i < count; ++i) {
+                    Node* current = q.front();
+                    row.push_back(current->val);
+                    nextCount += current->children.size();
+                    for (Node* child : current->children) {
+                        q.push(child);
+                    }
+                    q.pop();
+                }
+                count = nextCount;
+                result.push_back(row);
+            }
+        }
+        return result;
+    }
+
+    /**
      * Problem 559
      */
     int maxDepth(Node* root) {
