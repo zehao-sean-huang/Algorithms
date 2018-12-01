7b70eca0a974e2551a858ac90c8b1eaa07dafe0f
GodfatherSean <zehao.sean.huang@gmail.com>
2018-11-30 21:38:10 -0800

    leetcode 654

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -890,6 +890,33 @@ class Solution {
     }
 
     /**
+     * Problem 654
+     */
+    TreeNode* constructMaximumBinaryTreeHelper(vector<int>& nums, int l, int r) {
+        if (l > r) {
+            return nullptr;
+        } else if (l == r) {
+            return new TreeNode(nums[l]);
+        } else {
+            int index = 0, max = 0;
+            for (int i = l; i <= r; ++i) {
+                if (nums[i] > max) {
+                    index = i;
+                    max = nums[i];
+                }
+            }
+            TreeNode* root = new TreeNode(max);
+            root->left = constructMaximumBinaryTreeHelper(nums, l, index - 1);
+            root->right = constructMaximumBinaryTreeHelper(nums, index + 1, r);
+            return root;
+        }
+    }
+
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return constructMaximumBinaryTreeHelper(nums, 0, nums.size() - 1);
+    }
+
+    /**
      * Problem 700
      */
     TreeNode* searchBST(TreeNode* root, int val) {
