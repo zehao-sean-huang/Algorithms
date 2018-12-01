d71637d0b20228c14ceff6c9b7cd2705ae06b71f
GodfatherSean <zehao.sean.huang@gmail.com>
2018-11-30 22:38:41 -0800

    leetcode 637

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -980,6 +980,38 @@ class Solution {
     }
 
     /**
+     * Problem 637
+     */
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> result;
+        if (root != nullptr) {
+            int count = 1, nextCount = 0;
+            queue<TreeNode*> q;
+            q.push(root);
+            while (!q.empty()) {
+                nextCount = 0;
+                double sum = 0;
+                for (int i = 0; i < count; ++i) {
+                    TreeNode* current = q.front();
+                    sum += current->val;
+                    if (current->left != nullptr) {
+                        nextCount += 1;
+                        q.push(current->left);
+                    }
+                    if (current->right != nullptr) {
+                        nextCount += 1;
+                        q.push(current->right);
+                    }
+                    q.pop();
+                }
+                result.push_back(sum / count);
+                count = nextCount;
+            }
+        }
+        return result;
+    }
+
+    /**
      * Problem 654
      */
     TreeNode* constructMaximumBinaryTreeHelper(vector<int>& nums, int l, int r) {
