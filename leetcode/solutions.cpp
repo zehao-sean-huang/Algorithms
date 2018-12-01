e9d0559487be30cb2be5bffb9b92e822be0caddc
GodfatherSean <zehao.sean.huang@gmail.com>
2018-11-30 22:52:23 -0800

    leetcode 107

--- leetcode/solutions.cpp
+++ leetcode/solutions.cpp
@@ -673,6 +673,39 @@ class Solution {
     }
 
     /**
+     * Problem 107
+     */
+    vector<vector<int> > levelOrderBottom(TreeNode* root) {
+        vector<vector<int> > result;
+        if (root != nullptr) {
+            int count = 1;
+            queue<TreeNode*> q;
+            q.push(root);
+            while (!q.empty()) {
+                int nextCount = 0;
+                vector<int> row;
+                for (int i = 0; i < count; ++i) {
+                    TreeNode* current = q.front();
+                    row.push_back(current->val);
+                    if (current->left != nullptr) {
+                        nextCount += 1;
+                        q.push(current->left);
+                    }
+                    if (current->right != nullptr) {
+                        nextCount += 1;
+                        q.push(current->right);
+                    }
+                    q.pop();
+                }
+                count = nextCount;
+                result.push_back(row);
+            }
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+
+    /**
      * Problem 111
      */
     int minDepth(TreeNode* root) {
