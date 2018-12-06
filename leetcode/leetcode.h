a4ab03a0efd41f27f4a9e6ac58e12b01f7d2eb32
GodfatherSean <zehao.sean.huang@gmail.com>
2018-12-06 10:27:54 -0800

    leetcode TreeLinkNode Support

--- leetcode/leetcode.h
+++ leetcode/leetcode.h
@@ -15,6 +15,12 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
 class Node {
 public:
     int val;
