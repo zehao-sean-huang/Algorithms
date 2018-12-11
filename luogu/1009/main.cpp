ba4d85df2d9a9bc9188292d532fa7a59228baee7
Zehao Huang <1078351005@qq.com>
2018-12-10 16:24:22 -0800

    luogu 1009

--- luogu/1009/main.cpp
+++ luogu/1009/main.cpp
@@ -1,129 +1,79 @@
-// C++ includes used for precompiling -*- C++ -*-
-
-// Copyright (C) 2003-2014 Free Software Foundation, Inc.
-//
-// This file is part of the GNU ISO C++ Library.  This library is free
-// software; you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the
-// Free Software Foundation; either version 3, or (at your option)
-// any later version.
-
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// Under Section 7 of GPL version 3, you are granted additional
-// permissions described in the GCC Runtime Library Exception, version
-// 3.1, as published by the Free Software Foundation.
-
-// You should have received a copy of the GNU General Public License and
-// a copy of the GCC Runtime Library Exception along with this program;
-// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
-// <http://www.gnu.org/licenses/>.
-
-/** @file stdc++.h
- *  This is an implementation file for a precompiled header.
- */
-
-// 17.4.1.2 Headers
-
-// C
-#ifndef _GLIBCXX_NO_ASSERT
-#include <cassert>
-#endif
-#include <cctype>
-#include <cerrno>
-#include <cfloat>
-#include <ciso646>
-#include <climits>
-#include <clocale>
-#include <cmath>
-#include <csetjmp>
-#include <csignal>
-#include <cstdarg>
-#include <cstddef>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-
-#if __cplusplus >= 201103L
-#include <ccomplex>
-#include <cfenv>
-#include <cinttypes>
-#include <cstdalign>
-#include <cstdbool>
-#include <cstdint>
-#include <ctgmath>
-#include <cwchar>
-#include <cwctype>
-#endif
-
-// C++
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
-#include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <streambuf>
-#include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
-#include <vector>
-
-#if __cplusplus >= 201103L
-#include <array>
-#include <atomic>
-#include <chrono>
-#include <condition_variable>
-#include <forward_list>
-#include <future>
-#include <initializer_list>
-#include <mutex>
-#include <random>
-#include <ratio>
-#include <regex>
-#include <scoped_allocator>
-#include <system_error>
-#include <thread>
-#include <tuple>
-#include <typeindex>
-#include <type_traits>
-#include <unordered_map>
-#include <unordered_set>
-#endif
+#include <bits/stdc++.h>
 
 using namespace std;
-int n;
-string add(string first, string second) {
-    for ()
+
+string add(string str1, string str2) { 
+    if (str1.length() > str2.length()) 
+        swap(str1, str2); 
+    string str = ""; 
+    int n1 = str1.length(), n2 = str2.length(); 
+    reverse(str1.begin(), str1.end()); 
+    reverse(str2.begin(), str2.end()); 
+    int carry = 0; 
+    for (int i = 0; i < n1; i++) { 
+        int sum = ((str1[i] - '0') + (str2[i] - '0') + carry); 
+        str.push_back(sum % 10 + '0'); 
+        carry = sum / 10; 
+    } 
+    for (int i = n1; i < n2; i++) { 
+        int sum = ((str2[i] - '0') + carry); 
+        str.push_back(sum % 10 + '0'); 
+        carry = sum / 10; 
+    } 
+    if (carry) {
+        str.push_back(carry + '0');
+    }
+    reverse(str.begin(), str.end()); 
+    return str; 
+} 
+
+string multiply(string num1, string num2) { 
+    int n1 = num1.size(); 
+    int n2 = num2.size(); 
+    if (n1 == 0 || n2 == 0) 
+    return "0"; 
+    vector<int> result(n1 + n2, 0); 
+    int i_n1 = 0;  
+    int i_n2 = 0;  
+    for (int i=n1-1; i>=0; i--) { 
+        int carry = 0; 
+        int n1 = num1[i] - '0'; 
+        i_n2 = 0;  
+        for (int j=n2-1; j>=0; j--) { 
+            int n2 = num2[j] - '0'; 
+            int sum = n1*n2 + result[i_n1 + i_n2] + carry; 
+            carry = sum/10; 
+            result[i_n1 + i_n2] = sum % 10; 
+            i_n2++; 
+        } 
+        if (carry > 0) {
+            result[i_n1 + i_n2] += carry; 
+        }
+        i_n1++; 
+    } 
+    int i = result.size() - 1; 
+    while (i>=0 && result[i] == 0) {
+        i--;
+    }
+    if (i == -1) {
+        return "0";
+    }
+    string s = ""; 
+    while (i >= 0) {
+        s += std::to_string(result[i--]); 
+    }
+    return s; 
 }
+
 int main() {
+    int n = 0;
     cin >> n;
-    map<int, string> results;
-    results[0] = 1;
-    string result;
+    string r[n + 10];
+    string result = "1";
+    r[0] = r[1] = "1";
+    for (int i = 2; i <= n; ++i) {
+        r[i] = multiply(string(to_string(i)), r[i - 1]);
+        result = add(result, r[i]);
+    }
+    cout << result << endl;
 }
