c3b5805e5784f7eb5981122b237273ca4e4bf098
Zehao Huang <zehao.sean.huang@gmail.com>
2019-07-01 17:19:43 +0800

    hackerrank flipping-bits

---
+++ hackerrank/flipping-bits/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the flippingBits function below.
+long flippingBits(long n) {
+    return (u_int32_t) (-1 * n - 1);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        long n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        long result = flippingBits(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
