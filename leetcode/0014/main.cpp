0d7e3ae5ce1aed2b3242553f09c06afc9fa6eea9
Zehao Huang <zehao.sean.huang@gmail.com>
2019-07-03 11:37:03 +0800

    leetcode 0014

---
+++ leetcode/0014/main.cpp
@@ -0,0 +1,27 @@
+#include "../leetcode.h"
+
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
+    string prefix = strs[0];
+    for (int i = 1; i < strs.size(); ++i) {
+        while (strs[i].find(prefix) != 0) {
+            prefix = prefix.substr(0, prefix.size() - 1);
+            if (prefix.empty()) return "";
+        }
+    }
+    return prefix;
+}
+
+int main() {
+    #ifndef ONLINEJUDGE
+    freopen("main.in", "r", stdin);
+    #endif
+    int m = readNumber();
+    for (int i = 0; i < m; ++i) {
+        vector<string> strs = readStringVector();
+        cout << longestCommonPrefix(strs) << endl;
+    }
+    return 0;
+}
