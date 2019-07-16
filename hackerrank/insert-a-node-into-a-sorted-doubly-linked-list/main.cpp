3afb7a6a5da32cf7b6865d8acdd71b3c2bfd278d
Zehao Huang <zehao.sean.huang@gmail.com>
2019-07-16 13:40:04 +0800

    hackerrank insert-a-node-into-a-sorted-doubly-linked-list

---
+++ hackerrank/insert-a-node-into-a-sorted-doubly-linked-list/main.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the sortedInsert function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
+    if (head == nullptr) return new DoublyLinkedListNode(data);
+    if (head->next == nullptr) {
+        if (data > head->data) {
+            head->next = new DoublyLinkedListNode(data);
+            head->next->prev = head;
+        } else {
+            head->prev = new DoublyLinkedListNode(data);
+            head->prev->next = head;
+        }
+        return head;
+    }
+    DoublyLinkedListNode* temp = head->next;
+    if (head->prev == nullptr && data < head->data) {
+        head->prev = new DoublyLinkedListNode(data);
+        head->prev->next = head;
+        head = head->prev;
+    } else if (head->data <= data && data <= head->next->data) {
+        head->next = new DoublyLinkedListNode(data);
+        head->next->prev = head;
+        head->next->next = temp;
+        temp->prev = head->next;
+    } else {
+        temp = sortedInsert(temp, data);
+        head->next = temp;
+        temp->prev = head;
+    }
+    return head;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        DoublyLinkedList* llist = new DoublyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist->insert_node(llist_item);
+        }
+
+        int data;
+        cin >> data;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        DoublyLinkedListNode* llist1 = sortedInsert(llist->head, data);
+
+        print_doubly_linked_list(llist1, " ", fout);
+        fout << "\n";
+
+        free_doubly_linked_list(llist1);
+    }
+
+    fout.close();
+
+    return 0;
+}
