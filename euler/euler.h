37d22375605a7ab46f93aa62aa2ee3df3599063c
Sean Huang <zehao@berkeley.edu>
2020-04-06 02:57:01 -0700

    euler 051

--- euler/euler.h
+++ euler/euler.h
@@ -166,6 +166,15 @@ bool prime(ll n, set<ll> &primes) {
     return true;
 }
 
+bool prime(ll n) {
+    if (n == 1) return false;
+    if (n == 2) return true;
+    for (ll i = 2; i < sqrt(n); ++i) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 ll factorial(ll n) {
     if (n < 2) return 1;
     return n * factorial(n - 1);
